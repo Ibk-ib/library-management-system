14cf52366a8ab7a793188f608136aea2b576bd9c
Ibk-ib <abdullahibraheem211@gmail.com>
2026-09-22 22:08:38 +0100

    created book class, object, list, constructor

---
+++ Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Book
+{
+    public string Title { get; }
+    public string Author { get; }
+    public int PublicationYear { get; }
+    public bool IsOnLoan { get; set; }
+
+
+    public Book (string title, string author, int publicationYear)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+        {
+            throw new ArgumentException("An item must have a title.");
+        }
+
+        Title = title;
+        Author = author;
+        PublicationYear = publicationYear;
+    }
+
+    public Book(string title, string author)
+        : this(title, author, DateTime.Now.Year)
+    {
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 1925);
+book1.IsOnLoan = false;
+
+Book book2 = new Book("To Kill a Mockingbird", "Harper Lee", 1960);
+book2.IsOnLoan = false;
+
+Book book3 = new Book("1984", "George Orwell", 1949);
+book3.IsOnLoan = false;
+
+
+List<Book> books = new List<Book>();
+    books.Add(book1);
+    books.Add(book2);
+    books.Add(book3);
+
+foreach (Book book in books) {
+    Console.WriteLine("{0} ({1}) by {2} - {3}", book.Title, book.PublicationYear, book.Author, book.IsOnLoan ? "On Loan" : "Available");
+}
